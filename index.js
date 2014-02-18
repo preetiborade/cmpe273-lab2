71673751f5109e698524fe20bbabd664a5b71a1d
preetiborade <preeti.borade@gmail.com>
2014-02-18 18:48:32 +0530

    changes to the post and del methods
    Post method -
    retrieved the name and email from the request body and logged in with a
    new session_id (with the new name and email)
    
    Del method -
    Retrieved the session_id from the cookie and passed to the delete
    method which removes that entry from the sessionMap

--- index.js
+++ index.js
@@ -11,54 +11,70 @@ app.use(connect.query()); // Parse query string into `request.query`
 app.use('/', main);
 
 function main(request, response, next) {
-	switch (request.method) {
-		case 'GET': get(request, response); break;
-		case 'POST': post(request, response); break;
-		case 'DELETE': del(request, response); break;
-		case 'PUT': post(request, response); break;
-	}
+        switch (request.method) {
+                case 'GET': get(request, response); break;
+                case 'POST': post(request, response); break;
+                case 'DELETE': del(request, response); break;
+                case 'PUT': post(request, response); break;
+        }
 };
 
 function get(request, response) {
-	var cookies = request.cookies;
-	console.log(cookies);
-	if ('session_id' in cookies) {
-		var sid = cookies['session_id'];
-		if ( login.isLoggedIn(sid) ) {
-			response.setHeader('Set-Cookie', 'session_id=' + sid);
-			response.end(login.hello(sid));	
-		} else {
-			response.end("Invalid session_id! Please login again\n");
-		}
-	} else {
-		response.end("Please login via HTTP POST\n");
-	}
+        var cookies = request.cookies;
+        console.log(cookies);
+        if ('session_id' in cookies) {
+                var sid = cookies['session_id'];
+                if ( login.isLoggedIn(sid) ) {
+                        response.setHeader('Set-Cookie', 'session_id=' + sid);
+                        response.end(login.hello(sid));
+                } else {
+                        response.end("Invalid session_id! Please login again\n");
+                }
+        } else {
+                response.end("Please login via HTTP POST\n");
+        }
 };
 
 function post(request, response) {
-	// TODO: read 'name and email from the request.body'
-	// var newSessionId = login.login('xxx', 'xxx@gmail.com');
-	// TODO: set new session id to the 'session_id' cookie in the response
-	// replace "Logged In" response with response.end(login.hello(newSessionId));
 
-	response.end("Logged In\n");
+        var body = request.body;
+        var name = body['name'];
+        var email = body['email'];
+        var newSessionId = login.login(name, email);
+        response.setHeader('Set-Cookie','session_id=' + newSessionId);
+
+        // TODO: read 'name and email from the request.body'
+        // var newSessionId = login.login('xxx', 'xxx@gmail.com');
+        // TODO: set new session id to the 'session_id' cookie in the response
+        // replace "Logged In" response with response.end(login.hello(newSessionId));
+
+        response.end(login.hello(newSessionId));
 };
 
 function del(request, response) {
-	console.log("DELETE:: Logout from the server");
- 	// TODO: remove session id via login.logout(xxx)
- 	// No need to set session id in the response cookies since you just logged out!
+        var cookies = request.cookies;
+        login.logout(cookies['session_id']);
+        console.log("DELETE:: Logout from the server");
+        // TODO: remove session id via login.logout(xxx)
+        // No need to set session id in the response cookies since you just logged out!
 
-  	response.end('Logged out from the server\n');
+        response.end('Logged out from the server\n');
 };
 
-function put(request, response) {
-	console.log("PUT:: Re-generate new seesion_id for the same user");
-	// TODO: refresh session id; similar to the post() function
+function put(request, response){
 
-	response.end("Re-freshed session id\n");
+        
+
+
+        // TODO: refresh session id; similar to the post() function
+
+        response.end("Re-freshed session id\n");
+
 };
 
 app.listen(8000);
 
 console.log("Node.JS server running at 8000...");
+
+
+                                                                         
--- login.js
+++ login.js
@@ -1,25 +1,21 @@
-
-/**
- * Login Class
- */
 function Login() {
-	// sessionId -> user map
-	this.sessionMap = {
-		99999 : { name: 'Foo', email: 'foo@bar.com' }
-	};
+        // sessionId -> user map
+        this.sessionMap = {
+                99999 : { name: 'Foo', email: 'foo@bar.com' }
+        };
 }
 /**
  * Say Hello {name} to the user
  */
 Login.prototype.hello = function(sessionId) {
-	return 'Hello ' + this.sessionMap[sessionId].name + '\n';
+        return 'Hello ' + this.sessionMap[sessionId].name + '\n';
 };
 
 /**
  * Check whether the given session id is valid (is in sessionMap) or not.
  */
 Login.prototype.isLoggedIn = function(sessionId) {
-	return sessionId in this.sessionMap;
+        return sessionId in this.sessionMap;
 };
 
 /**
@@ -27,25 +23,27 @@ Login.prototype.isLoggedIn = function(sessionId) {
  */
 Login.prototype.login = function(_name, _email) {
    /*
-	* Generate unique session id and set it into sessionMap like foo@bar.com
-	*/
-	var sessionId = new Date().getTime();
-	this.sessionMap[sessionId] = { name: _name, email: _email } 
-	
-	console.log('new session id ' + sessionId + ' for login::' + _email);
-	
-	return sessionId;
+        * Generate unique session id and set it into sessionMap like foo@bar.com
+        */
+        var sessionId = new Date().getTime();
+        this.sessionMap[sessionId] = { name: _name, email: _email }
+
+        console.log('new session id ' + sessionId + ' for login::' + _email);
+
+        return sessionId;
 };
 
 /**
  * Logout from the server
- */ 
+ */
 Login.prototype.logout = function(sessionId) {
-	console.log('logout::' + sessionId);
-   /*
-	* TODO: Remove the given sessionId from the sessionMap
-	*/
-};
 
-// Export the Login class
-module.exports = new Login();
+        var session_ID = sessionId;
+        delete this.sessionMap[session_ID];
+        console.log('logged out!!!!');
+
+   /*
+        * TODO: Remove the given sessionId from the sessionMap
+        */
+                                                                              
+}
